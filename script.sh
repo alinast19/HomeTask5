82b62d79ced2aa309dbb180184e1c9c59f8fddd6
Alina <alina.st1919@gmail.com>
2022-11-27 14:37:14 +0100

    fix: problems with start

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 mkdir /var/folder{1,2}
 
-cat /vagrant/files-mover.service /lib/systemd/system/files-mover.service
+cp /vagrant/files-mover.service /lib/systemd/system/files-mover.service
 
 systemctl daemon-reload
 systemctl start files-mover
