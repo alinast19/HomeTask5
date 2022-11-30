ca7277d51d7e286cf820bcd68cb4f61b35d640fa
Alina <alina.st1919@gmail.com>
2022-11-30 19:49:14 +0100

    rewrite 5 lab

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 mkdir /var/folder{1,2}
 
-cat /vagrant/files-mover.service /etc/systemd/system/files-mover.service
+cat /vagrant/files-mover.service > /etc/systemd/system/files-mover.service
 
 systemctl daemon-reload
 systemctl start files-mover
