4daf0366bd5957f5ad6e85126107835ecaa80829
Alina <alina.st1919@gmail.com>
2022-11-30 19:53:41 +0100

    feat: fix file names

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@ Vagrant.configure("2") do |config|
     hw5.vm.hostname = "hw5"
     hw5.vm.box = "ubuntu/focal64"
     hw5.vm.provision "file", source: "files-mover.service", destination: "~/files-mover.service"
-
+  
     config.vm.provision "shell", path: "script.sh"
   end
 end
--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
-mkdir /var/folder{1,2}
+mkdir /folder{1,2}
 
 cat /vagrant/files-mover.service > /etc/systemd/system/files-mover.service
 
 systemctl daemon-reload
-systemctl start files-mover
-systemctl enable files-mover
+systemctl start files-mover.service
+systemctl enable files-mover.service
